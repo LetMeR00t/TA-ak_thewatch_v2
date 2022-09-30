4396301a46cfb34e0292b5c887b0cf54fe85eccf
LmR <letmer00t@gmail.com>
2022-09-30 13:39:38 +0200

    perf: remove unused code

--- TA-ak_thewatch_v2/bin/ta_ak_thewatch_v2/modalert_the_watch_v2_create_notable_events_helper.py
+++ TA-ak_thewatch_v2/bin/ta_ak_thewatch_v2/modalert_the_watch_v2_create_notable_events_helper.py
@@ -1,20 +1,3 @@
-import os
-import sys
-import re
-
-if sys.version_info[0] < 3:
-    py_version = "aob_py2"
-else:
-    py_version = "aob_py3"
-
-ta_name = 'TA-ak_thewatch_v2'
-ta_lib_name = 'ta_ak_thewatch_v2'
-pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
-new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]
-new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name]))
-new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name, py_version]))
-sys.path = new_paths
-
 import json
 import re
 import ipaddress
